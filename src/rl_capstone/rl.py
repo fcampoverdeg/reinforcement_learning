15195d1c31a4c0187f36ea10efc7a8f873ba8df9
houndsito <fcampoverdeg@gmail.com>
2025-10-24 03:12:06 -0400

    Finished implementing the Q-Learning of my practice code for learning purposes, solved a coding problem with dictionaries, learned about dictionary comprehension and sorted list of tuples

--- src/rl_capstone/rl.py
+++ src/rl_capstone/rl.py
@@ -4,7 +4,7 @@ While getting used to Numpy and MatplotLib, machine learning terminology, etc.
 """
 
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 '''
@@ -97,3 +97,120 @@ During each episode, the agent selects actions and updates its Q-Table using the
 total_rewards -> tracks cumulative rewards per episode
 epsilon -> decays gradually to reduce randomness over time.
 '''
+
+rewards_all_episodes = []
+
+for episode in range(num_episodes):
+    state = start
+    total_rewards = 0
+    done = False
+    
+    while not done:
+        action_index = choose_action(state)
+        action = actions[action_index]
+
+        next_state = (state[0] + action[0], state[1] + action[1])
+
+        if not is_valid(next_state):
+            reward = reward_fire
+            done = True
+        elif next_state == goal:
+            reward = reward_goal
+            done = True
+        else:
+            reward = reward_step
+
+        old_value = Q[state][action_index]
+        next_max = np.max(Q[next_state]) if is_valid(next_state) else 0
+
+        Q[state][action_index] = old_value + alpha * \
+            (reward + gamma * next_max - old_value)
+        
+        state = next_state
+        total_rewards = reward
+
+    # global epsilon
+    epsilon = max(0.01, epsilon * 0.995)
+    rewards_all_episodes.append(total_rewards)
+
+'''
+Step 5: Extract the Optimal path after Training
+
+-> This function follows the highest Q-values at each state to extract the best path.
+-> It stops when the goal is reached or no valid next moves are available
+-> The visited set prevents cycles.
+
+'''
+
+def get_optimal_path (Q, start, goal, actions, maze, max_steps=200):
+    path = [start]
+    state = start
+    visited = set()
+
+    for _ in range(max_steps):
+        if state == goal:
+            break
+        visited.add(state)
+
+        best_action = None
+        best_value = -float('inf')
+
+        for idx, move in enumerate(actions):
+            next_state = (state[0] + move[0], state[1] + move[1])
+
+            if (0 <= next_state[0] < maze.shape[0] and
+                0 <= next_state[1] < maze.shape[1] and
+                maze[next_state] == 0 and
+                    next_state not in visited):
+
+                if Q[state][idx] > best_value:
+                    best_value = Q[state][idx]
+                    best_action = idx
+
+        if best_action is None:
+            break
+
+        move = actions[best_action]
+        state = (state[0] + move[0], state[1] + move[1])
+        path.append(state)
+
+    return path
+
+optimal_path = get_optimal_path(Q, start, goal, actions, maze)
+                
+'''
+Step 6: Visualize the Maze, Robot Path, Start and Goal
+
+-> The maze and path are visualized using a calmig green color palette.
+-> The start and goal positions are visually highlighted.
+-> The learned path is drawn clearly to demonstrate the agent's solution.
+
+'''
+
+def plot_maze_with_path(path):
+    cmap = ListedColormap(['#eef8ea', '#a8c79c'])
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(maze, cmap=cmap)
+
+    plt.scatter(start[1], start[0], marker='o', color='#81c784', edgecolors='black',
+                s=200, label='Start (Robot)', zorder=5)
+    plt.scatter(goal[1], goal[1], marker='*', color='#388e3c', edgecolors='black',
+                s=300, label='Goal (Diamond)', zorder=5)
+    
+    rows, cols = zip(*path)
+    plt.plot(cols, rows, color='#60b37a', linewidth=4,
+             label='Learned Path', zorder=4)
+    
+    plt.title('Reinforcement Learning: Robot Maze Navigation')
+    plt.gca().invert_yaxis()
+    plt.xticks(range(maze.shape[1]))
+    plt.yticks(range(maze.shape[0]))
+    plt.grid(True, alpha=0.2)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+plot_maze_with_path(optimal_path)
+
+    
